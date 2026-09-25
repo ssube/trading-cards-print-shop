135b5242bfa36c8f8348a8525810484221601b8d
Sean Sube <seansube@gmail.com>
2026-09-25 10:15:44 -0500

    Keep online tabs visible in offline demo

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -14,3 +14,4 @@ import './card-library.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
 import './mobile-card.css'
+import './demo-nav.css'
